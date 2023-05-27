0e3969bfa7cf83b67acf5f092c60c90b044a96b3
Sagar Talagatti <sagartalagatti@gmail.com>
2023-05-27 15:59:43 +0530

    Completed the lecture 'Refactoring and Deriving States'

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,55 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  //using ref or state depends on what we plan to do with the entered value. If we only want it once when form is submitted, ref is better. However, say if we need value of field on every keystroke for kind of instant validation, then using state is better idea.
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim().length !== 0;
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    //this prevents the default behaviour of sending HTTP request on form submission
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+
+    setEnteredName("");
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = !nameInputIsInvalid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
